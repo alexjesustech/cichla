eeb05667f5d7aa883b972cf579907ca9926a0e01
Alex Jesus <alexjesus.tech@gmail.com>
2023-11-12 00:06:33 -0400

    Intalação do Bootstrap 5 (pastas resources)

---
+++ resources/js/Pages/Bootstrap.vue
@@ -0,0 +1,14 @@
+<script setup>
+import {Head} from "@inertiajs/vue3";
+</script>
+
+<template>
+    <Head title="Bootstrap" />
+    <div class="container py-4 px-3 mx-auto">
+        <h1>Hello, Bootstrap and Vite!</h1>
+        <button class="btn btn-primary">Primary button</button>
+    </div>
+</template>
+
+<style>
+</style>
